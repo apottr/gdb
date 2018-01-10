78e4e9ffd3175c3c596b4176243bfbb5cd963b25
Amelia <apottr@akira.local>
2018-01-10 00:29:06 -0500

    worked more on query

--- query.go
+++ query.go
@@ -1,13 +1,39 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "regexp"
+)
 
+func Query_InsertHandler(q map[string]string) error {
+  
+}
 
+func Query_FindHandler(q map[string]string) error {
+  
+}
 
-func QueryParser(qtext string) {
-  re := regexp.MustCompile("'.+'|\".+\"|[^,]+")
-  s := r.FindAllString(qtext,-1)
-  fmt.Printf("%#v")
+func QueryParser(qtext string) error {
+  var err error
+  re := regexp.MustCompile("'.+'|\".+\"|[^,]*")
+  s := re.FindAllString(qtext,-1)
+  q := map[string]string{
+    "command": s[0],
+    "graph": s[1],
+    "edge": s[2],
+    "node": s[3]
+  }
+  switch q["command"] {
+    case "insert":
+      err = Query_InsertHandler(q)
+    case "query":
+      err = Query_FindHandler(q)
+  }
+
+  if err != nil {
+    return err
+  }
+  return nil
 }
 
 func (db *Db) Query (q string) error {
